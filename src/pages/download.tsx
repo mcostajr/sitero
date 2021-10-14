89fbc7ba7bd4c308d59ba4d8c61068ec880c611e
Mauro Freire <maurofreirejr@gmail.com>
2021-10-14 10:33:50 -0300

    update download page

--- src/pages/download.tsx
+++ src/pages/download.tsx
@@ -1,6 +1,5 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import { FiDownload } from 'react-icons/fi';
 
 import styles from '../styles/Download.module.scss';
 
@@ -10,92 +9,21 @@ export default function Download() {
             <Head>
                 <title>SiteRO | Download</title>
             </Head>
-            <div className={styles.wrapper}>
-                
-                <div className={styles.title}>
-                    <h1>Download</h1>
-                    <p>Baixe o nosso jogo e se divirta muito, jogo totalmente gratuito.</p>
-                </div>
-                <div className={styles.downloadContainer}>
-                    <ul className={styles.downloadList}>
-                        <li className={styles.downloadItem}>
-                            <div className={styles.image}>
-                                <Image 
-                                    src="/download1.png"
-                                    alt="download1"
-                                    width={200}
-                                    height={100}
-                                />
-                            </div>
-                            <div className={styles.info}>
-                                <div>
-                                    <span>Download Completo</span>
-                                    <span>1.2 Gb</span>
-                                </div>
-                                <button className={styles.button}><FiDownload/> Mega</button>
-                            </div>
-                        </li>
-                        <li className={styles.downloadItem}>
-                            <div className={styles.image}>
-                                <Image 
-                                    src="/download2.png"
-                                    alt="download2"
-                                    width={200}
-                                    height={100}
-                                />
-                            </div>
-                            <div className={styles.info}>
-                                <div>
-                                    <span>Download Completo</span>
-                                    <span>1.2 Gb</span>
-                                </div>
-                                <button className={styles.button}><FiDownload/> Google Drive</button>
-                            </div>
-                        </li>
-                        <li className={styles.downloadItem}>
-                            <div className={styles.image}>
-                                <Image 
-                                    src="/download3.png"
-                                    alt="download3"
-                                    width={200}
-                                    height={100}
-                                />
-                            </div>
-                            <div className={styles.info}>
-                                <div>
-                                    <span>Download Completo</span>
-                                    <span>1.2 Gb</span>
-                                </div>
-                                <button className={styles.button}><FiDownload/> Torrent</button>
-                            </div>
-                        </li>
-                        <li className={styles.downloadItem}>
-                            <div className={styles.image}>
-                                <Image 
-                                    src="/download4.png"
-                                    alt="download4"
-                                    width={200}
-                                    height={100}
-                                />
-                            </div>
-                            <div className={styles.info}>
-                                <div>
-                                    <span>Download Completo</span>
-                                    <span>1.2 Gb</span>
-                                </div>
-                                <button className={styles.button}><FiDownload/> Media Fire</button>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            
-            </div>
             <div className={styles.background}>
-                <Image
-                    src='/download.png' 
-                    alt='download'
-                    width='450px'
-                    height='820px'
+                <div className={styles.wrapper}>
+                    <h1>DOWNLOADS</h1>
+                    <p>Baixe o nosso jogo e divirta-se muito, jogo totalmente gratuito</p>
+                    <div className={styles.buttonContainer}>
+                        <button>BAIXAR</button>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.photo}>
+                <Image 
+                    src="/cartoon.png"
+                    alt="cartoon"
+                    layout="fill"
+                    objectFit="contain"
                 />
             </div>
         </main>
